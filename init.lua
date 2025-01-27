eb30f16c32b4f4dff1e2767a5daa026df05fb609
Karol <dobrekkarol@gmail.com>
2025-01-27 19:52:17 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,7 @@
+require("karol.lazy")
+require("mason").setup()
+require("lualine").setup()
+vim.wo.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.cmd.colorscheme("catppuccin")
