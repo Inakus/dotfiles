05d523754b7e665641b5ddd6042fde599edc1032
Karol <dobrekkarol@gmail.com>
2025-01-28 17:08:15 +0100

    status bar change

--- init.lua
+++ init.lua
@@ -5,3 +5,6 @@ vim.wo.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.cmd.colorscheme("catppuccin")
+vim.opt.tabstop = 2 -- Liczba spacji dla tab
+vim.opt.shiftwidth = 2 -- Liczba spacji dla wcięć
+vim.opt.expandtab = true -- Konwertuj tab na spacje
