1829ae0c518623a17d92cc5fa5d9930a86b3a9eb
Karol <dobrekkarol@gmail.com>
2025-02-17 22:28:00 +0100

    change some plugins and keymaps

--- lua/karol/keymaps.lua
+++ lua/karol/keymaps.lua
@@ -9,3 +9,9 @@ vim.keymap.set("v", ">", ">gv", opts)
 --Buffer change
 vim.keymap.set("n", "<C-n>", ":bn<CR>", opts)
 vim.keymap.set("n", "<C-p>", ":bp<CR>", opts)
+
+--Jump and center
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "<C-f>", "<C-f>zz", opts)
+vim.keymap.set("n", "<C-b>", "<C-b>zz", opts)
--- lua/karol/plugins/lualine.lua
+++ lua/karol/plugins/lualine.lua
@@ -5,6 +5,14 @@ return {
 		local lualine = require("lualine")
 		lualine.setup({
 			options = { theme = "catppuccin", section_separators = "", component_separators = "" },
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
 		})
 	end,
 }
