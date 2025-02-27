7e70d5983985e49df1d4aa35600b404f0869b942
Karol <dobrekkarol@gmail.com>
2025-02-27 23:53:59 +0100

    some changes

--- lua/karol/plugins/lspconfig.lua
+++ lua/karol/plugins/lspconfig.lua
@@ -140,6 +140,23 @@ return {
 					},
 				})
 			end,
+			["gopls"] = function()
+				local util = require("lspconfig.util")
+				lspconfig["gopls"].setup({
+					cmd = { "gopls" },
+					filetypes = { "go", "gomod", "gowork", "gotmpl" },
+					root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+					settings = {
+						gopls = {
+							completeUnimported = true,
+							usePlaceholders = true,
+							analyses = {
+								unusedparams = true,
+							},
+						},
+					},
+				})
+			end,
 		})
 	end,
 }
